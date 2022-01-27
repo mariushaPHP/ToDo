5c7b32abe802639c7f2869b5b9d42c6d7427a815
mariushaPHP <marius.zilinskas@stud.kitm.lt>
2022-01-27 10:59:16 +0200

    first commit

--- www/app/Validation.php
+++ www/app/Validation.php
@@ -3,50 +3,18 @@
 namespace ToDo;
 
 class Validation{
-    private $data;
-    private $errors = [];
-    private static $fields = ['subject', 'priority', 'dueDate'];
-
-    public function __construct($post_data){
-        $this->data = $post_data;
-    }
-
-    public function validateForm(){
-        foreach (self::$fields as $field){
-
-            if(!array_key_exists($field, $this->data)){
-                trigger_error("$field nesuvestas");
-                //return;
-            }
+    private static $errors = [];
+    public static function validator($post){
+        if(strlen(trim($post['subject'])) == ''){
+            self::$errors[] = 'Neįvesta užduotis';
         }
-        $this->validateSubject();
-        $this->validatePriority();
-        $this->validateDate();
-        return $this->errors;
-    }
-
-    public function validateSubject(){
-        $value = $this->data['subject'];
-        if(empty($value)){
-            $this->addError('subject','Neįvesta užduotis!');
+        if(!array_key_exists('priority', $post)){
+            self::$errors[] = 'Nepasirinktas prioritetas';
         }
-    }
-
-    public function validatePriority(){
-        $value = $this->data['priority'];
-        if(empty($value)){
-            $this->addError('priority','Nepasirinktas prioritetas!');
+        if($post['dueDate'] == ''){
+            self::$errors[] = 'Nepasirinkta data';
         }
-    }
 
-    public function validateDate(){
-        $value = $this->data['dueDate'];
-        if(empty($value)){
-            $this->addError('dueDate','Nepasirinkta data!');
-        }
-    }
-
-    public function addError($key, $value){
-        $this->errors[$key] = $value;
+        return self::$errors;
     }
 }
--- www/controllers/add-task.php
+++ www/controllers/add-task.php
@@ -4,15 +4,15 @@ use ToDo\Task;
 use ToDo\Validation;
 
 if(isset($_POST['save'])){
-      var_dump($_POST);
-//    $validation = new Validation($_POST);
-//    $errors = $validation->validateForm();
+    var_dump($_POST);
+    $errors = Validation::validator($_POST);
     //echo 'belenkas' . var_dump($errors);
-//    if(empty($errors)){
-//        $connection = Database::connect(); // prisijungimas prie db
-//        $task = new Task($connection);    // sukuria task objekta
-//        $task->createTask($_POST);         //
-//    }
+    if(empty($errors)){
+        $connection = Database::connect(); // prisijungimas prie db
+        $task = new Task($connection);    // sukuria task objekta
+        $task->createTask($_POST);         //
+    }
+
 }
 
 require 'view/pages/add-task.view.php';
